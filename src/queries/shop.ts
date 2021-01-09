48ddb1efc22ff5a9c201e1b383b52fb31f5360ac
juan <jpqokubo@gmail.com>
2021-01-09 17:36:11 -0500

    feat(shop): added query data

--- src/queries/shop.ts
+++ src/queries/shop.ts
@@ -8,6 +8,21 @@ export const getShop = gql`
         code
         country
       }
+      logUrl
+      subdomain
+      companyId
+      companyAddress {
+        streetAddress1
+        streetAddress2
+        city
+        cityArea
+        postalCode
+        country
+        countryArea
+        phone
+        lat
+        lng
+      }
       countries {
         country
         code
